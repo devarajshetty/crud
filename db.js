e3292452e8062b25b6388700ff8e5dcb6a570283
devaraj <devaraj@pofitec.com>
2018-12-04 21:49:44 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,7 @@
+const mongoose  =  require('mongoose');
+mongoose.connect('mongodb://localhost:27017/cruddb',(err)=>{
+    if(!err)
+        console.log('Mongo Db Connected');
+    else
+        console.log('Error'+ JSON.stringify(err, undefined,2));
+});
