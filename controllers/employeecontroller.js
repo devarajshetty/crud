0e94b4570962dfbb9237c2af119e0a644a12dca5
devaraj <devaraj@pofitec.com>
2018-12-10 20:32:33 +0530

    dec10

---
+++ controllers/employeecontroller.js
@@ -0,0 +1,14 @@
+const express = require('express');
+var router = express.Router();
+
+
+var { Employee } = require('../models/employee');
+
+// => localhost:3000/employees/
+router.get('/list', (req, res) => {
+    Employee.find((err, docs) => {
+        if (!err) { res.send(docs); }
+        else { console.log('Error in Retriving Employees :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
+ module.exports = router;
